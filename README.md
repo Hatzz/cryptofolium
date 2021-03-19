fca6aa93355a65b1734b1446640d764e2716e503
Hatzz <git@hatzz.io>
2021-03-19 12:47:30 +0100

    Update readme and add links to coingecko and github repository

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
 # Cryptofolium
 
+https://cryptofolium.app  
 A simple app to track the worth of your coins.  
 Built using the coingecko api, vue, tailwindcss and heroicons.  
 Thank you for the favicon Vitaly Gorbachev [link](https://www.flaticon.com/free-icon/nodes_2165676)
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,43 @@
 <template>
   <div class="flex flex-col">
+    <div class="absolute flex">
+      <a
+        href="https://www.coingecko.com/en/api"
+        target="_blank"
+      ><img
+          src="../assets/coingecko.svg"
+          alt="Coingecko"
+          class="w-12 p-2"
+        ></a>
+
+      <a
+        href="https://github.com/Hatzz/cryptofolium/"
+        target="_blank"
+      >
+        <img
+          src="../assets/github.svg"
+          alt="github"
+          class="w-12 p-2"
+        >
+      </a>
+    </div>
     <h1 class="text-4xl text-gray-50 py-12 text-center">${{ holdingsValue }}</h1>
-    <Holding :openHolding="openHolding" @open="openHolding = $event" v-for="(holding, id) in holdings" :key="id" :id="id" />
-    <router-link class="p-4 ml-auto" :to="{ name: 'Search' }">
-      <img src="../assets/plus.svg" class="bg-gray-800 p-3 w-11 rounded-full" alt="">
+    <Holding
+      :openHolding="openHolding"
+      @open="openHolding = $event"
+      v-for="(holding, id) in holdings"
+      :key="id"
+      :id="id"
+    />
+    <router-link
+      class="p-4 ml-auto"
+      :to="{ name: 'Search' }"
+    >
+      <img
+        src="../assets/plus.svg"
+        class="bg-gray-800 p-3 w-11 rounded-full"
+        alt=""
+      >
     </router-link>
   </div>
 </template>
@@ -43,5 +77,4 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-
 </style>
